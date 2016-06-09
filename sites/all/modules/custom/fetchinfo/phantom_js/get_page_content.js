cc09c8c60350657faf63b7600013383fbbf97bd5
DongFlexxia <dong@flexxia.ca>
2016-06-09 18:47:43 -0400

    dev updateFields

--- sites/all/modules/custom/fetchinfo/phantom_js/get_page_content.js
+++ sites/all/modules/custom/fetchinfo/phantom_js/get_page_content.js
@@ -2,6 +2,23 @@ var url = 'http://program.gxtv.cn';
 var page = require('webpage').create();
 
 page.open(url, function() {
-  console.log(page.content);
+  // get page content
+  // var pageContent = page.content;
+  // console.log(pageContent);
+
+  // write string content to file
+  var fs = require('fs');
+  var filePath = 'output.txt';
+
+  if (fs.isWritable(filePath)) {
+    console.log('"' + filePath + '" is writable.');
+
+    var content = 'Hello World!';
+    fs.write(filePath, content, 'w');
+  }
+  else {
+    console.log('"' + filePath + '" is NOT writable.');
+  }
+
   phantom.exit();
 })
